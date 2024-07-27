f7dad82409d3c301977ea5c25db067aa1aa7c4de
CMCode2001 <cheikh.mb2022@gmail.com>
2024-07-27 02:55:57 +0000

    Initiation avec les routes | Express

--- NODE-POKEMON-API/app.js
+++ NODE-POKEMON-API/app.js
@@ -1,1 +1,15 @@
-console.log("hello,Node ! ");
+const express = require('express')
+const pokemons = require('../mock-pokemon')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => res.send(`Hello fils !`))
+// ADD PREMIER ROUTE 
+app.get('/api/pokemons/:id/', (req,res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id ===id )
+    res.send(`Pokemon name :${pokemon.name}`)
+})
+
+app.listen(port, () => console.log(`App is starting to : http://localhost:${port}`))
